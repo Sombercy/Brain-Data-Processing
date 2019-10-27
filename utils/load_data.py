4e989bb9044c3cf374eb048068ce939580a14c3c
Oksana <32892779+Sombercy@users.noreply.github.com>
2019-10-27 10:04:45 +0300

    Update load_data.py

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 24 15:28:10 2019
-
-@author: savenko
-"""
 from scipy.io import loadmat
 import pandas as pd
 
-def load_data(filename, ftype = 'csv'):
-    if ftype == 'csv': 
+def load_data(filename, index = False, binary = True):
+    if filename[-3:] == 'csv': 
         df = pd.read_csv(filename)
-        X = df.drop(['y'], axis = 1)
-        y = df['y']
-    elif ftype == 'mat':
-        df = loadmat(full_name, squeeze_me=True)
-        X = df['X']
-        y = df['y']
+        if index: 
+            df = df.drop(df.columns[0], axis=1)
+        X = df.drop(['y'], axis = 1).values
+        y = df['y'].values
+        if not binary:
+            # binarize target
+            y[y>1] = 0
+    elif filename[-3:] == 'mat':
+        df = loadmat(filename, squeeze_me=True)
+        X = df['X'].values
+        y = df['y'].values
+        if not binary:
+            # binarize target
+            y[y>1] = 0
     return X, y
- 
-        
-
-        
-    
