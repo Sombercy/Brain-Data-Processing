368c71ed90561d54c4cf7ac03b59b186a1a27936
Oksana <32892779+Sombercy@users.noreply.github.com>
2019-11-04 12:39:10 +0300

    Update train_test.py

--- utils/train_test.py
+++ utils/train_test.py
@@ -13,9 +13,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 import numpy as np
 import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectKBest, f_classif
 
-
-def fit_predict(X, y):
+def fit_predict(X, y, selection = None, k = None):
     """
     This function returns maximum accuracy score, maximum roc_auc score and 
     size of feature vector
@@ -25,10 +26,27 @@ def fit_predict(X, y):
     X_scaled = scaler.fit_transform(X)
     scoring = 'roc_auc'
     max_auc = 0
+    svc = SVC(kernel='linear', gamma='auto', C=1, class_weight='balanced')
+    if selection:
+    # Define the dimension reduction to be used.
+    # Here we use a classical univariate feature selection based on F-test,
+    # namely Anova. We set the number of features to be selected to 500
+    
+        delta = X_scaled.shape[1]//20
+        max_score  = 0
+        best_k = 0
+        for k in range(delta, X.shape[1], delta):
+            feature_selection = SelectKBest(f_classif, k=k)
+            anova_svc = Pipeline([('anova', feature_selection), ('svc', svc)])
+            score = cross_val_score(anova_svc, X_scaled, y, cv=KFold(n_splits=10).split(X, y), scoring=scoring, n_jobs=-1)
+            if np.mean(score) > max_score: 
+                max_score = np.mean(score)
+                best_k = k
+            if np.mean(score) > max_auc: max_auc = np.mean(max_score)
+        print('Mean ANOVA_SVC ROC_AUC: ', max_score, ' with %s features' % best_k )
     # Evaluate on different Classifiers
-    score = cross_val_score(SVC(kernel='linear', gamma='auto',
-                               C=1, class_weight='balanced'), 
-                     X_scaled, y, cv=KFold(n_splits=10).split(X, y), scoring=scoring, n_jobs=-1)
+    score = cross_val_score(svc, X_scaled, y, cv=KFold(n_splits=10).split(X, y),
+                            scoring=scoring, n_jobs=-1)
     if np.mean(score) > max_auc: max_auc = np.mean(score)
     print('Mean SVC ROC_AUC: ', np.mean(score))
     print('SVC ROC_AUC std: ', np.std(score))
@@ -46,6 +64,23 @@ def fit_predict(X, y):
     scoring = 'accuracy'
     max_acc = 0
     # Evaluate on different Classifiers
+    if selection:
+    # Define the dimension reduction to be used.
+    # Here we use a classical univariate feature selection based on F-test,
+    # namely Anova. We set the number of features to be selected to 500
+        feature_selection = SelectKBest(f_classif, k=k)
+        anova_svc = Pipeline([('anova', feature_selection), ('svc', svc)])
+
+        delta = X_scaled.shape[1]//20
+        max_score  = 0
+        best_k = 0
+        for k in range(delta, X.shape[1], delta):
+            score = cross_val_score(anova_svc, X_scaled, y, cv=KFold(n_splits=10).split(X, y), scoring=scoring, n_jobs=-1)
+            if np.mean(score) > max_score: 
+                max_score = np.mean(score)
+                best_k = k
+            if np.mean(score) > max_auc: max_auc = np.mean(max_score)
+        print('Mean ANOVA_SVC Accuracy: ', max_score, ' with %s features' % best_k )
     score = cross_val_score(SVC(kernel='linear', gamma='auto',
                                 C=1, class_weight='balanced'), 
                      X_scaled, y, cv=KFold(n_splits=10).split(X, y), scoring=scoring, n_jobs=-1)
@@ -62,5 +97,5 @@ def fit_predict(X, y):
     if np.mean(score) > max_acc: max_acc = np.mean(score)
     print('Mean DT Accuracy: ', np.mean(score))
     print('DT Accuracy std: ', np.std(score))
-    
-    return max_acc, max_auc, X.shape[1]
+
+    return 0
